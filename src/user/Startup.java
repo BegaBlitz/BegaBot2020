6b83165c41f1067cac5dab83d322c242e7020f43
simplyCyclops <maipez04@gmail.com>
2019-12-08 20:18:56 +0200

    created project and inited hardware

--- src/user/Startup.java
+++ src/user/Startup.java
@@ -1,9 +1,20 @@
 package user;
 
+import robot.hardware.chassis.RobotChassis;
+import robot.hardware.motors.LargeMotor;
+import robot.hardware.sensors.ColorSensor;
+import robot.hardware.sensors.GyroSensor;
+
 public class Startup {
 	
 	public static void createHardware() {
-		//declare your hardware here
+		new RobotChassis("leftWheel",'B', "rightWheel", 'C');
+		new LargeMotor("arm", 'A');
+		
+		new ColorSensor("leftColor", 4);
+		new ColorSensor("rightColor", 1);
+		
+		new GyroSensor("gyro", 3);
 	}
 	
 	public static void createRuns() {
