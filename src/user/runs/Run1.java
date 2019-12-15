9b323fab7e3ea2fa665dfd9d5d439d27e5bbea13
johnmeshulam <johnmeshulam@gmail.com>
2019-12-15 20:31:17 +0200

    15.12.19

--- src/user/runs/Run1.java
+++ src/user/runs/Run1.java
@@ -14,26 +14,25 @@ public class Run1 extends RobotRun {
 		RobotMap.getSensor("gyro").resetToCurrentValue();
 		//
 		GyroFollower.followDegrees(0.5, 0.5, 0, 0.008,1345, false);
-		
-		//1
-		RobotMap.getChassis().tankDriveDegrees(-0.7, -0.7, 0.4, 205, true);
 		//2
-		GyroFollower.followDegrees(0.15, 0.3, -70, 0.008, 35, true);
+		RobotMap.getChassis().tankDriveDegrees(-0.7, -0.7, 0.4, 205, true);
 		//3
-		RobotMap.getChassis().tankDriveDegrees(-0.4,-0.4,0.4,466,true);
+		GyroFollower.followDegrees(0.15, 0.3, -70, 0.008, 35, true);
 		//4
-		GyroFollower.followDegrees(0.15, 0.3, 52, 0.008, 50, true);
+		RobotMap.getChassis().tankDriveDegrees(-0.4,-0.4,0.4,463,true);
 		//5
-		GyroFollower.followDegrees(0.35, 0.35, 0, 0.008, 420, true);
+		GyroFollower.followDegrees(0.15, 0.3, 52, 0.008, 45, true);
 		//6
-		RobotMap.getChassis().tankDriveDegrees(0.01, 0.01, 0.4, 207, true);
+		GyroFollower.followDegrees(0.35, 0.35, 0, 0.008, 420, true);
 		//7
-		//Wait.waitForSeconds(1.0);
+		RobotMap.getChassis().tankDriveDegrees(0.05, 0.01, 0.4, 235, true);
 		//8
-		//RobotMap.getChassis().tankDriveDegrees(-0.7, -0.7, 0.4, 350, false);
+		//Wait.waitForSeconds(1.0);
 		//9
+		//RobotMap.getChassis().tankDriveDegrees(-0.7, -0.7, 0.4, 350, false);
+		//10
 		//GyroFollower.followDegrees(0.5, 0.5, 60, 0.008, 60, true);
 		//RobotMap.getChassis().tankDriveDegrees(-0.6,- 0.6, 0.5,900, true);
-		//10
+		//11
 	}
 }
