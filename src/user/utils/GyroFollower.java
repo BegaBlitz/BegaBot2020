b77b96412ec4ced87cfd12999ce2751b1217092a
johnmeshulam <johnmeshulam@gmail.com>
2019-12-23 20:28:35 +0200

    עדכוני ג'יירו

--- src/user/utils/GyroFollower.java
+++ src/user/utils/GyroFollower.java
@@ -4,14 +4,13 @@ import robot.RobotMap;
 import robot.runs.RunHandler;
 
 public class GyroFollower {
-	
-	public static void followDegrees(double p0, double ecceleration, int target, double kp, int distance, boolean brake){ {
+
+	public static void followDegreesForward(double p0, double acceleration, int target, double kp, int distance, boolean brake){
 		
 		RobotMap.getMotor("leftWheel").resetEncoder();
 		RobotMap.getMotor("rightWheel").resetEncoder();
 		
 		RobotMap.getChassis().drive(p0);
-		
 		while(RunHandler.isRunning() && RobotMap.getMotor("leftWheel").readEncoder() <= distance
 				&& RobotMap.getMotor("rightWheel").readEncoder() <= distance) {
 
@@ -28,7 +27,28 @@ public class GyroFollower {
 			
 		
 	}
-		
+
+	public static void followDegreesBackwards(double p0, double acceleration, int target, double kp, int distance,
+			boolean brake) {
+
+			RobotMap.getMotor("leftWheel").resetEncoder();
+			RobotMap.getMotor("rightWheel").resetEncoder();
+
+			RobotMap.getChassis().drive(p0);
+			while (RunHandler.isRunning() && RobotMap.getMotor("leftWheel").readEncoder() >= distance
+					&& RobotMap.getMotor("rightWheel").readEncoder() >= distance) {
+
+				int error = (int) (RobotMap.getSensor("gyro").read() - target);
+				System.out.println(RobotMap.getSensor("gyro").read());
+				RobotMap.getChassis().tankDrive(p0, Math.min(p0 + (error * kp), 1.0));
+			}
+
+			if (brake) {
+				RobotMap.getChassis().brake();
+			} else {
+				RobotMap.getChassis().coast();
+			}
+
 	}
-	
+
 }
